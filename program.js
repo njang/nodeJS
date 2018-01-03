9e67b10a3a839bd7cd22a31ec1f65976224984e7
njang <neo.w.jang@gmail.com>
2018-01-03 15:09:28 -0600

    Finish exercise 2

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD');
+// Exercise 1 of 12
+// console.log('HELLO WORLD');
+
+// Exercise 2 of 12
+let sum = 0;
+for (let i = 2; i < process.argv.length; i++) {
+  sum += Number(process.argv[i]);
+}
+console.log(sum);
